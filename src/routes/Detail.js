059d3f44d012f7d9634823db29c0bbf637020d12
AUTUMN <krshlee22@naver.com>
2023-03-02 21:39:49 +0900

    react-movieApp

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+import {useParams} from "react-router-dom"
+
 function Detail() {
+    const [loading, setLoading] = useState(true);
+    const {id} = useParams()
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json.data.movie)
+        setLoading(false);
+    }
+    useEffect(() => {
+        getMovie();
+    }, [])
     return (
-        <h1>Detail</h1>
+        <div>
+            {loading ? <h1>Loading...</h1> :
+            <h1>Detail</h1>
+            }
+        </div>
     )
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from 'react';
-import Movie from './components/Movie';
+import Movie from '../components/Movie';
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async() => {
-    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year`)
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
+    try{
+        const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year`)
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    } catch (err) {
+        throw err;
+    }
   }
   useEffect(() => {
     getMovies()
   }, [])
+
   return (
     <div>
       {
@@ -21,7 +26,8 @@ function Home() {
         {movies.map(item =>
           <Movie 
             key={item.id}
-            medium_cover_image={item.medium_cover_image}
+            id={item.id}
+            medium_cover_image={item.large_cover_image}
             title={item.title}
             summagy={item.summary}
             genres={item.genres}
